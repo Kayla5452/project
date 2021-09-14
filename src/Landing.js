7ad9b6cca2db87d9088c90f7f2be7806480de7e3
30019578 <71050359+30019578@users.noreply.github.com>
2021-09-14 17:03:14 +1200

    Update Landing.js

--- src/Landing.js
+++ src/Landing.js
@@ -2,7 +2,7 @@ function Landing(){
     return(
         <div className="card">
             <div className="card-body">
-                <h2>Bundy Trackers</h2>
+                <h2 className="LandingTitle">Bundy Trackers</h2>
                 <p>Welcome to the official Bundy Trackers™ website. The ‘top-of-the-line’ general industry timesheet management and creation system for the workplace.</p>
                 <hr/>
                 <p>To get started and document any new employee to the company or look for any existing employee information, navigate to the ‘Employee Information’ tab at the top of the screen and click on the desired link seen in the drop-down menu.</p>
